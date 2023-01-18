34010be95f7706e8344c66fdf1b6adb73ec719db
Виталий Чопик <leo.space97@gmail.com>
2023-01-18 12:27:03 +0300

    fix: изменил файлы

---
+++ wordpress/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+  <div class="footer__container">
+    <div class="footer__content">
+      <a class="footer__logo" href="#">
+        <img src="images/icons/logo-white.svg" alt="Kroyyork">
+      </a>
+      <div class="footer__column footer__column_address">
+        <h3 class="footer__title">Адрес нашего шоу-рума / оптового отдела</h3>
+        <p class="footer__text">г. Москва, Бережковская наб., д. 20, стр. 64</p>
+        <p class="footer__text">Пн-Пт.: с 09:00 до 18:00</p>
+      </div>
+      <div class="footer__column footer__column_contact">
+        <h3 class="footer__title">Телефон и e-mail отдела оптовых закупок</h3>
+        <a href="tel:84951422707" class="footer__link">8 495 142-27-07</a>
+        <a href="mailto:kroyyork@dol.ru" class="footer__link">kroyyork@dol.ru</a>
+      </div>
+      <div class="footer__social">
+        <a href="#" class="footer__social-link footer__social-link_whatsapp">
+          <img src="images/whatsapp.png" alt="Whatsapp">
+        </a>
+        <a href="#" class="footer__social-link footer__social-link_facebook">
+          <img src="images/icons/facebook.svg" alt="Facebook">
+        </a>
+      </div>
+    </div>
+    <div class="footer__bottom">
+      <p class="footer__text-bottom footer__text-bottom_copyright">© kroyyork, 2021, все права защищены</p>
+      <p class="footer__text-bottom footer__text-bottom_policy">Политика конфиденциальности</p>
+      <p class="footer__text-bottom footer__text-bottom_design">Дизайн сайта - @_web_create</p>
+    </div>
+  </div>
+</footer>
+  </div>
+  <?php wp_footer();?>
+</body>
+
+</html>
