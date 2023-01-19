96b7e4846452c0a1a4497b51aeb4f772a6c0a6ce
Виталий Чопик <leo.space97@gmail.com>
2023-01-19 10:54:23 +0300

    feat: добавил новые секции

--- wordpress/index.php
+++ wordpress/index.php
@@ -60,6 +60,201 @@
           </div>
         </div>
       </section>
+      <section class="about page__about">
+        <div class="about__container">
+          <div class="about__image image-decor-ibg about__image_left">
+            <img src="<?php echo get_template_directory_uri() ?>/images/about/01.png" alt="Women">
+          </div>
+          <div class="about-content about__content">
+            <h2 class="title about-content__title">О нас</h2>
+            <div class="about-content__grid">
+              <div class="about-content__item">
+                <span class="about-content__item-strong">30 лет</span>
+                <span class="about-content__item-text">
+                  специализируемся на производстве женских пальто
+                </span>
+              </div>
+              <div class="about-content__item">
+                <span class="about-content__item-strong">более 100</span>
+                <span class="about-content__item-text">
+                  моделей пальто в 20 цветах и оттенках
+                </span>
+              </div>
+              <div class="about-content__item">
+                <span class="about-content__item-strong">свыше 6</span>
+                <span class="about-content__item-text">
+                  эксклюзивных составов: альпака сури, альпака беби сури, лама, шерсть, кашемир
+                </span>
+              </div>
+              <div class="about-content__item">
+                <span class="about-content__item-strong">> 250 тыс</span>
+                <span class="about-content__item-text">
+                  женщин от Мурманска до Владивостока приобрели пальто KROYYORK и остались довольны своим выбором
+                </span>
+              </div>
+              <div class="about-content__item">
+                <span class="about-content__item-text">только лучшие ткани из</span>
+                <span class="about-content__item-strong">итальянского</span>
+                <span class="about-content__item-text">
+                  региона Прато известного своими качественными текстильными мануфактурами
+                </span>
+              </div>
+              <div class="about-content__item">
+                <span class="about-content__item-text">пальто производятся на хорошо оборудованной</span>
+                <span class="about-content__item-strong">европейской</span>
+                <span class="about-content__item-text">
+                  фабрике
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="about__image image-decor-ibg about__image_right">
+            <img src="<?php echo get_template_directory_uri() ?>/images/about/02.png" alt="Women">
+          </div>
+        </div>
+      </section>
+      <section class="lama page__lama">
+        <div class="lama__container">
+          <img class="lama__image-bg" src="<?php echo get_template_directory_uri() ?>/images/lama-bg.png" alt="Lama">
+          <h2 class="title lama__title">Пальто из шерсти альпака</h2>
+          <p class="lama__desc">Уникальные свойства шерсти альпака сури и альпака бэби сури удовлетворят запросы ваших
+            самых взыскательных клиентов, ведь она:</p>
+          <div class="lama__grid">
+            <div class="lama__item">
+              <div class="lama__icon">
+                <img src="<?php echo get_template_directory_uri() ?>/images/lama/01.png" alt="в 7 раз теплее овечьей шерсти">
+              </div>
+              <p class="lama__text">в 7 раз теплее овечьей шерсти</p>
+            </div>
+            <div class="lama__item">
+              <div class="lama__icon">
+                <img src="<?php echo get_template_directory_uri() ?>/images/lama/02.png" alt="гипоаллергенна ">
+              </div>
+              <p class="lama__text">гипоаллергенна </p>
+            </div>
+            <div class="lama__item">
+              <div class="lama__icon">
+                <img src="<?php echo get_template_directory_uri() ?>/images/lama/03.png" alt="устойчива к влаге">
+              </div>
+              <p class="lama__text">устойчива к влаге</p>
+            </div>
+            <div class="lama__item">
+              <div class="lama__icon">
+                <img src="<?php echo get_template_directory_uri() ?>/images/lama/04.png" alt="отлично держит форму">
+              </div>
+              <p class="lama__text">отлично держит форму</p>
+            </div>
+            <div class="lama__item">
+              <div class="lama__icon">
+                <img src="<?php echo get_template_directory_uri() ?>/images/lama/05.png" alt="прочная, но легкая">
+              </div>
+              <p class="lama__text">прочная, но легкая</p>
+            </div>
+            <div class="lama__item">
+              <div class="lama__icon">
+                <img src="<?php echo get_template_directory_uri() ?>/images/lama/06.png" alt="с приятной бархатистой фактурой">
+              </div>
+              <p class="lama__text">с приятной бархатистой фактурой</p>
+            </div>
+          </div>
+          <h3 class="lama__subtitle">И, наконец, это просто красиво!</h3>
+          <button class="button lama__button" type="button">Получить каталог + прайс-лист</button>
+        </div>
+      </section>
+      <section class="gallery page__gallery">
+        <div class="gallery__container">
+          <div class="swiper slider-auto gallery__slider">
+            <div class="swiper-wrapper gallery__wrapper">
+              <div class="swiper-slide gallery__slide-image-ibg">
+                <img src="<?php echo get_template_directory_uri() ?>/images/gallery/01.png" alt="Women">
+              </div>
+              <div class="swiper-slide gallery__slide-image-ibg">
+                <img src="<?php echo get_template_directory_uri() ?>/images/gallery/02.png" alt="Women">
+              </div>
+              <div class="swiper-slide gallery__slide-image-ibg">
+                <img src="<?php echo get_template_directory_uri() ?>/images/gallery/03.png" alt="Women">
+              </div>
+              <div class="swiper-slide gallery__slide-image-ibg">
+                <img src="<?php echo get_template_directory_uri() ?>/images/gallery/04.png" alt="Women">
+              </div>
+              <div class="swiper-slide gallery__slide-image-ibg">
+                <img src="<?php echo get_template_directory_uri() ?>/images/gallery/05.png" alt="Women">
+              </div>
+              <div class="swiper-slide gallery__slide-image-ibg">
+                <img src="<?php echo get_template_directory_uri() ?>/images/gallery/06.png" alt="Women">
+              </div>
+              <div class="swiper-slide gallery__slide-image-ibg">
+                <img src="<?php echo get_template_directory_uri() ?>/images/gallery/07.png" alt="Women">
+              </div>
+            </div>
+            <div class="swiper-pagination gallery__progress-bar"></div>
+          </div>
+        </div>
+      </section>
+      <section class="page__testi testi">
+        <div class="testi__container">
+          <h2 class="title testi__title">Отзывы партнеров</h2>
+          <div class="swiper slider-auto testi__slider">
+            <div class="swiper-wrapper testi__wrapper">
+              <div class="swiper-slide testi__slide">
+                <div class="testi__slide-title">ИП Пивень Ольга Игоревна</div>
+                <p class="testi__slide-text">
+                  Красноярский край, г. Зеленогорск магазин КАШЕМИР
+                </p>
+                <span class="testi__slide-devider"></span>
+                <p class="testi__slide-text testi__slide-text_scroll">
+                  Являюсь партнером этой уникальной компании с 2013 года и ценю её сотрудников за лояльность и
+                  готовность всегда идти на встречу ;)) Желаю компании только процветания, чтобы можно было и дальше
+                  успешно сотрудничать.
+                </p>
+              </div>
+              <div class="swiper-slide testi__slide">
+                <div class="testi__slide-title">ИП Красноглазова Татьяна Дмитриевна</div>
+                <p class="testi__slide-text">
+                  г. Тула, магазин "Фасон" ТРЦ " Рио", ТК "Парадиз"
+                </p>
+                <span class="testi__slide-devider"></span>
+                <p class="testi__slide-text testi__slide-text_scroll">
+                  Являюсь партнером этой уникальной компании с 2013 года и ценю её сотрудников за лояльность и
+                  готовность всегда идти на встречу ;)) Желаю компании только процветания, чтобы можно было и дальше
+                  успешно сотрудничать.
+                </p>
+              </div>
+              <div class="swiper-slide testi__slide">
+                <div class="testi__slide-title">ИП Салихова Файруза Фаварисовна</div>
+                <p class="testi__slide-text">
+                  г.Нефтекамск, магазин GLANC
+                </p>
+                <span class="testi__slide-devider"></span>
+                <p class="testi__slide-text testi__slide-text_scroll">
+                  С Кройорк работаем 7 лет. Достойные коллекции и надёжность – вот главные качества компании. А
+                  сотрудники лояльные, внимательные и добропорядочные. С самого начала работаем с Екатериной и очень
+                  рады этому.
+                </p>
+              </div>
+              <div class="swiper-slide testi__slide">
+                <div class="testi__slide-title">Наталия Оглезнева, собственник</div>
+                <p class="testi__slide-text">
+                  «Специализированный магазин Пальто», г. Хабаровск
+                </p>
+                <span class="testi__slide-devider"></span>
+                <p class="testi__slide-text testi__slide-text_scroll">
+                  Kroyyork является надежным партнером сети Специализированных магазинов Пальто более 12 лет. При
+                  открытии нового розничного магазина в г. Хабаровск вопрос работать ли с брендом Kroyyork даже не стоял
+                  – конечно, работать! Kroyyork из года в год обеспечивает безупречное качество производимых изделий:
+                  отличный состав тканей, идеальная посадка, регулярное обновление коллекции с учетом последних трендов.
+                  Продукция бренда занимает основную часть ассортимента сети Специализированных магазинов Пальто.
+                  Kroyyork неизменно подтверждает свое лидерство на рынке, как ведущий российский производитель пальто
+                  премиального сегмента. Стабильность, ответственность, внимание к деталям, индивидуальный подход к
+                  клиенту – это отличительные черты команды Kroyyork. Работать с Kroyyork приятно и легко. От всего
+                  коллектива мы желаем Kroyyork процветания, развития, стабильного роста!
+                </p>
+              </div>
+            </div>
+            <div class="swiper-pagination testi__progress-bar"></div>
+          </div>
+        </div>
+      </section>
       <section class="contact-form page__contact-form">
         <div class="contact-form__container">
           <div class="contact-form__image-ibg">
